feade9270094c25b607b9b039907cc23846eca3e
tuxudo <tuxudo@users.noreply.github.com>
2018-11-14 07:52:59 -0500

    Added audit migration (#3)
    * Added audit migration
    
    Bug found by @joncrain

---
+++ migrations/2018_11_14_000001_laps_audits.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Laps extends Migration
+{
+    public function up()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->create('laps', function (Blueprint $table) {
+            $table->longText('audit')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table('laps', function (Blueprint $table) {
+            $table->dropColumn('audit');
+        });
+    }
+}
