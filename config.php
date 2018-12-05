e3052561c946fde26f6f1a5e0ebdd69936ba0c50
tuxudo <tuxudo@users.noreply.github.com>
2018-12-05 18:25:09 -0500

    Fixed config

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ return [
 	|
 	*/
 
-	$conf['laps_encryption_key'] =  getenv_default('LAPS_ENCRYPTION_KEY', '');
-	$conf['laps_password_decrypt_enabled'] = getenv_default('LAPS_PASSWORD_DECRYPT_ENABLED', TRUE, 'bool');
-	//$conf['view_laps_password'] = getenv_default('VIEW_LAPS_PASSWORD', [''], 'array');
+	$conf['laps_encryption_key'] =  getenv_default('LAPS_ENCRYPTION_KEY', ''),
+	$conf['laps_password_decrypt_enabled'] = getenv_default('LAPS_PASSWORD_DECRYPT_ENABLED', TRUE, 'bool'),
+	//$conf['view_laps_password'] = getenv_default('VIEW_LAPS_PASSWORD', [''], 'array'),
 ];
